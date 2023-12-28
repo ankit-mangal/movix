7cabd4c94ea7f86cab13dafac87fba1251e6f510
Ankit Mangal <mangalankit843@gmail.com>
2023-12-29 00:36:35 +0530

    adding boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
-function App() {
-  useEffect(() => {
-    apiTesting();
-  }, []);
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) =>
-      console.table(res.results)
-    );
-  };
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import SearchResult from "./pages/searchResult/SearchResult";
 
-  return <div className="App">Hello</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.scss";
+
 const Details = () => {
   return <div>Details</div>;
 };
